b604dbeefde17cc35001faa2430372d6dbe843b5
Bilal Shahzad <bilal.shahzad@ibexglobal.com>
2020-01-28 23:25:16 +0500

    file is encrypted on upload and decrypted before sending to user on download

--- Part 5 - Code/SampleCode/WebApplication7/ApiControlers/FilesDataController.cs
+++ Part 5 - Code/SampleCode/WebApplication7/ApiControlers/FilesDataController.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Net.Http.Headers;
+using System.Security.Cryptography;
 using System.Web;
 using System.Web.Http;
 using WebApplication7.Models;
@@ -41,7 +42,9 @@ namespace WebApplication7.ApiControlers
                             var fileSavePath = System.IO.Path.Combine(rootPath, fileDTO.FileUniqueName + fileDTO.FileExt);
 
                             // Save the uploaded file to "UploadedFiles" folder
-                            file.SaveAs(fileSavePath);
+                            //file.SaveAs(fileSavePath);
+
+                            EncDecHelper.EncryptFile(file.InputStream, fileSavePath);
 
                             //Save File Meta data in Database
 
@@ -73,9 +76,10 @@ namespace WebApplication7.ApiControlers
                 HttpResponseMessage response = new HttpResponseMessage(HttpStatusCode.OK);
                 var fileFullPath = System.IO.Path.Combine(rootPath, fileDTO.FileUniqueName + fileDTO.FileExt);
 
-                byte[] file = System.IO.File.ReadAllBytes(fileFullPath);
-                System.IO.MemoryStream ms = new System.IO.MemoryStream(file);
+                //byte[] file = System.IO.File.ReadAllBytes(fileFullPath);
+                //System.IO.MemoryStream ms = new System.IO.MemoryStream(file);
 
+                byte[] file = EncDecHelper.DecryptFile(fileFullPath);
                 response.Content = new ByteArrayContent(file);
                 response.Content.Headers.ContentDisposition = new ContentDispositionHeaderValue("attachment");
                 //String mimeType = MimeType.GetMimeType(file); //You may do your hard coding here based on file extension
@@ -91,5 +95,104 @@ namespace WebApplication7.ApiControlers
             }
 
         }
+    }
+
+    public static class EncDecHelper
+    {
+        private static string EncryptionKey = "MAKV2SPBNI99212";
+        public static void EncryptFile(string inputFilePath, string outputfilePath)
+        {
+            using (Aes encryptor = Aes.Create())
+            {
+                Rfc2898DeriveBytes pdb = new Rfc2898DeriveBytes(EncryptionKey, new byte[] { 0x49, 0x76, 0x61, 0x6e, 0x20, 0x4d, 0x65, 0x64, 0x76, 0x65, 0x64, 0x65, 0x76 });
+                encryptor.Key = pdb.GetBytes(32);
+                encryptor.IV = pdb.GetBytes(16);
+                using (FileStream fsOutput = new FileStream(outputfilePath, FileMode.Create))
+                {
+                    using (CryptoStream cs = new CryptoStream(fsOutput, encryptor.CreateEncryptor(), CryptoStreamMode.Write))
+                    {
+                        using (FileStream fsInput = new FileStream(inputFilePath, FileMode.Open))
+                        {
+                            int data;
+                            while ((data = fsInput.ReadByte()) != -1)
+                            {
+                                cs.WriteByte((byte)data);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        public static void EncryptFile(Stream fsInput, string outputfilePath)
+        {
+            using (Aes encryptor = Aes.Create())
+            {
+                Rfc2898DeriveBytes pdb = new Rfc2898DeriveBytes(EncryptionKey, new byte[] { 0x49, 0x76, 0x61, 0x6e, 0x20, 0x4d, 0x65, 0x64, 0x76, 0x65, 0x64, 0x65, 0x76 });
+                encryptor.Key = pdb.GetBytes(32);
+                encryptor.IV = pdb.GetBytes(16);
+                using (FileStream fsOutput = new FileStream(outputfilePath, FileMode.Create))
+                {
+                    using (CryptoStream cs = new CryptoStream(fsOutput, encryptor.CreateEncryptor(), CryptoStreamMode.Write))
+                    {
+                        using (fsInput)
+                        {
+                            int data;
+                            while ((data = fsInput.ReadByte()) != -1)
+                            {
+                                cs.WriteByte((byte)data);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        public static void DecryptFile(string inputFilePath, string outputfilePath)
+        {
+            using (Aes encryptor = Aes.Create())
+            {
+                Rfc2898DeriveBytes pdb = new Rfc2898DeriveBytes(EncryptionKey, new byte[] { 0x49, 0x76, 0x61, 0x6e, 0x20, 0x4d, 0x65, 0x64, 0x76, 0x65, 0x64, 0x65, 0x76 });
+                encryptor.Key = pdb.GetBytes(32);
+                encryptor.IV = pdb.GetBytes(16);
+                using (FileStream fsInput = new FileStream(inputFilePath, FileMode.Open))
+                {
+                    using (CryptoStream cs = new CryptoStream(fsInput, encryptor.CreateDecryptor(), CryptoStreamMode.Read))
+                    {
+                        using (FileStream fsOutput = new FileStream(outputfilePath, FileMode.Create))
+                        {
+                            int data;
+                            while ((data = cs.ReadByte()) != -1)
+                            {
+                                fsOutput.WriteByte((byte)data);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        public static byte[] DecryptFile(string inputFilePath)
+        {
+            using (Aes encryptor = Aes.Create())
+            {
+                Rfc2898DeriveBytes pdb = new Rfc2898DeriveBytes(EncryptionKey, new byte[] { 0x49, 0x76, 0x61, 0x6e, 0x20, 0x4d, 0x65, 0x64, 0x76, 0x65, 0x64, 0x65, 0x76 });
+                encryptor.Key = pdb.GetBytes(32);
+                encryptor.IV = pdb.GetBytes(16);
+                using (FileStream fsInput = new FileStream(inputFilePath, FileMode.Open))
+                {
+                    using (CryptoStream cs = new CryptoStream(fsInput, encryptor.CreateDecryptor(), CryptoStreamMode.Read))
+                    {
+                        MemoryStream ms = new MemoryStream();
+                        cs.CopyTo(ms);
+                        return ms.ToArray();
+                    }
+                }
+            }
+
+            return null;
+        }
+
+
     }
 }
